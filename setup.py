2fd366fe4fab3fa42cfc8faeeb8d9d7ad1b08503
Lorenzo Mele <lorenzo@loman.it>
2017-03-11 19:34:43 +0100

    adding setup to make it installable via pip

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+setup(
+  name = 'pychatbot',
+  packages = ['pychatbot'],
+  version = '0.1',
+  description = 'A lib to create chatbots',
+  author = 'Lorenzo Mele',
+  author_email = 'greenkey@loman.it',
+  keywords = ['chatbot', 'telegram'], 
+)
