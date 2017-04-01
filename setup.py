58cf44609a7f2135492a4741ae2cbcb57721f356
Lorenzo Mele <lorenzo@loman.it>
2017-04-01 17:24:40 +0200

    version upgrade

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name='pychatbot',
     packages=['pychatbot', 'pychatbot.endpoints'],
-    version='0.6.2',
+    version='0.6.3',
     description='A lib to create chatbots',
     author='Lorenzo Mele',
     author_email='greenkey@loman.it',
