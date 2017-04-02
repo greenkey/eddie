ecc1a5a2e120d003b3dc4ea540b74aba788a76ac
Lorenzo Mele <lorenzo@loman.it>
2017-04-02 18:22:04 +0200

    added a lot of info in setup.py, aligned other files

--- setup.py
+++ setup.py
@@ -1,13 +1,53 @@
-from distutils.core import setup
+#!/usr/bin/env python
+
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+import sys
+import io
+
+import pychatbot
+
+
+def read(filename):
+    with open(filename) as file:
+        return file.read()
+
+
+def get_requirements(req):
+    if req.startswith('-r'):
+        for subreq in get_requirements(req.split()[1]):
+            yield subreq
+    else:
+        yield req
 
 
 setup(
     name='pychatbot',
-    packages=['pychatbot', 'pychatbot.endpoints'],
-    version='0.7.0',
+    version=pychatbot.__version__,
+    author=pychatbot.__author__,
+    author_email=pychatbot.__email__,
     description='A lib to create chatbots',
-    author='Lorenzo Mele',
-    author_email='greenkey@loman.it',
+    long_description=read('README.md'),
+    url='http://github.com/greenkey/pychatbot/',
+    packages=['pychatbot', 'pychatbot.endpoints'],
+    include_package_data=True,
+    platforms='any',
     keywords=['chatbot', 'telegram'],
-    install_requires=['python-telegram-bot'],
+    tests_require=get_requirements('-r requirements-dev.txt'),
+    install_requires=get_requirements('-r requirements.txt'),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Natural Language :: English',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
