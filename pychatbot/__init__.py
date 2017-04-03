877ae84e7831327b3f39bcbdc01ee773299ca067
Lorenzo Mele <lorenzo@loman.it>
2017-04-03 22:21:04 +0200

    linting

--- pychatbot/__init__.py
+++ pychatbot/__init__.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 
+""" Main module of pychatbot it contains the following modules:
+
+    * bot, the Bot class itself, used to create your bot
+    * endpoints, the classes to connect to bot services
+"""
+
 __author__ = """Lorenzo Mele"""
 __email__ = 'greenkey@loman.it'
 __version__ = '0.7.0'
--- pychatbot/endpoints/__init__.py
+++ pychatbot/endpoints/__init__.py
@@ -1,2 +1,8 @@
+""" This module is meant to contain all the endpoints class.
+
+	An endpoint is a connection to a bot service, i.e.: Telegram, Facebook
+	Messenger, Twitter, Slack...
+"""
+
 from .http import HttpEndpoint
 from .telegram import TelegramEndpoint
